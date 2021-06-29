3f0ab0af65e3ce9c9d9e2e1b3ee763bc4b859941
Yasith Fonseka <86307603+YasithSFonseka@users.noreply.github.com>
2021-06-29 16:16:55 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const connectDB = require("./config/db");
+
+const productroutes = require('./routes/productRoutes');
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/products', productroutes );
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
